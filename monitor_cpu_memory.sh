4669f468e27c5d86fa804ce03dd1384b6651c2da
McBain <cwma.campbell@gmail.com>
2023-02-15 10:57:49 +0000

    Update monitor_cpu_memory.sh

--- monitor_cpu_memory.sh
+++ monitor_cpu_memory.sh
@@ -1,4 +1,4 @@
 #!/bin/bash/env
 
-ps -eo pcpu,pmem,pid,user,args|awk 'BEGIN {print "%COU\t", "%MEMORY\t", "%USER"}
+ps -eo pcpu,pmem,pid,user,args|awk 'BEGIN {print "%CPU\t", "%MEMORY\t", "%USER"}
 {OFS="\t"} /rstudio/ && /tier/ && !/bash/ {print $1, $2, $3}'
