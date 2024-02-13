f9544acc11817c92e0eafe99d4a4c061f70bc413
McBain <cwma.campbell@gmail.com>
2024-02-13 14:56:46 +0000

    Update monitor_cpu_memory.sh

--- monitor_cpu_memory.sh
+++ monitor_cpu_memory.sh
@@ -4,4 +4,4 @@ ps -eo pcpu,pmem,pid,user,args|awk 'BEGIN {print "%CPU\t", "%MEMORY\t", "%USER"}
 {OFS="\t"} /rstudio/ && /tier/ && !/bash/ {print $1, $2, $3}'
 
 # creates a better output
-ps -eo user,pmem,args | awk 'BEGIN {print @USER\t", "%MEMORY} {OFS="\t"} /rstudio/ && /tier/ && !/(bash|awk)/ {print $1,$2,$3} {sum_var +=$2}; END {print "\n\t\tTOTAL %MEMORY\n\t\t",sum_var};'
+ps -eo user,pmem,args | awk 'BEGIN {print "@USER\t", "%MEMORY"} {OFS="\t"} /rstudio/ && /tier/ && !/(bash|awk)/ {print $1,$2,$3} {sum_var +=$2}; END {print "\n\t\tTOTAL %MEMORY\n\t\t",sum_var};'
