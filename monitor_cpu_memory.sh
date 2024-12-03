001bfdcd62fd9bc8d12cb7cd7136649428b549ea
McBain <cwma.campbell@gmail.com>
2024-12-03 17:27:40 +0000

    Update monitor_cpu_memory.sh

--- monitor_cpu_memory.sh
+++ monitor_cpu_memory.sh
@@ -12,4 +12,7 @@ ps -eo user,pmem,args | awk 'BEGIN {print "@USER\t", "%MEMORY"} {OFS="\t"} /rstu
 # captures the pid and username - using the fields of awk
 ps -eo pcpu,pmem,pid,user,args|awk 'BEGIN {print "%CPU\t", "%MEMORY", "PID\t", "%USER"} /rstudio/ && /tier/ && !/bash/ {print $1"\t", $2"\t", $3"\t", $7}'
 
+# describe the process... in this example, rsession
+ps -C rsession -eo user,pid,cmd,lstart,etime
+
 ps aux --sort=-%mem | head
