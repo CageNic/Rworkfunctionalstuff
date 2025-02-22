eff36192d13190c957ee4d32bd3129ab7247da10
McBain <cwma.campbell@gmail.com>
2025-02-22 09:31:39 +0000

    Update arrow_filter_dates.R

--- arrow_filter_dates.R
+++ arrow_filter_dates.R
@@ -2,6 +2,9 @@
 # filter dates using Arrow #
 ############################
 
+# note that this also applies to dataframes and tibbles using dplyr, you do not need to use the mutate function when filtering
+# this is because filter() can operate on temporary transformations, so you can apply as.Date directly to the column in the filter function itself
+
 library(arrow)
 
 df <- data.frame(
@@ -16,3 +19,15 @@ arrow_df |>
     Date >= as.Date('2024-03-01'), 
     Date <= as.Date('2024-06-01')
   )
+
+# tibble
+
+library(dplyr)
+
+data <- tibble(
+  id = 1:5,
+  date_column = c("2025-02-20", "2025-02-21", "2025-02-22", "2025-02-23", "2025-02-24")
+)
+
+filtered_data <- data %>%
+  filter(as.Date(date_column) >= as.Date("2025-02-22"))
