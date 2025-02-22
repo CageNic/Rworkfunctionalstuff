b31270c0747b8546c09f4a67b588c6e4e4f0f865
McBain <cwma.campbell@gmail.com>
2025-02-22 09:42:05 +0000

    Update arrow_filter_dates.R

--- arrow_filter_dates.R
+++ arrow_filter_dates.R
@@ -9,15 +9,15 @@ library(arrow)
 
 df <- data.frame(
   ID = 1:10,
-  Date = seq(as.Date("2024-01-01"), by = "month", length.out = 10),
+  Date = seq(as.Date("1901-01-01"), by = "month", length.out = 10),
 )
 
 arrow_df <- open_dataset(tf)
 
 arrow_df |> 
   filter(
-    Date >= as.Date('2024-03-01'), 
-    Date <= as.Date('2024-06-01')
+    Date >= as.Date('1901-03-01'), 
+    Date <= as.Date('1902-06-01')
   )
 
 # tibble
